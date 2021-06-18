df65fe6054410891fdb2480a16ba83e31a99cc38
Garrett-Bodley <Garrett.Bodley@gmail.com>
2021-06-18 15:47:29 -0400

    Archive Flatiron code.

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -1,3 +1,67 @@
 class FiguresController < ApplicationController
-  # add controller methods
+  get '/figures' do
+    @figures = Figure.all
+    erb :'/figures/index'
+  end
+
+  get '/figures/new' do
+    @titles = Title.all
+    @landmarks = Landmark.all
+    # @landmarks = Landmark.all.select{|landmark| landmark.figure == nil}
+    erb :'/figures/new'
+  end
+
+  get '/figures/:id' do
+    @figure = current_figure
+    erb :'/figures/show'
+  end
+
+  get '/figures/:id/edit' do
+    @figure = current_figure
+    @landmarks = Landmark.all
+    @titles = Title.all
+    erb :'/figures/edit'
+  end
+
+  patch '/figures/:id'do
+    @figure = current_figure
+    @figure.update(params[:figure])
+    if !params[:title][:name].empty?
+      @title = Title.create(params[:title])
+      @figure.titles << @title
+    end
+    if !params[:landmark][:name].empty?
+      @landmark = Landmark.create(params[:landmark])
+      @figure.landmarks << @landmark
+    end
+    redirect "/figures/#{@figure.id}"
+  end
+
+  post '/figures' do
+    @figure = Figure.create(params[:figure])
+    if !params[:title][:name].empty?
+      @title = Title.create(params[:title])
+      @figure.titles << @title
+    end
+    if !params[:landmark][:name].empty?
+      @landmark = Landmark.create(params[:landmark])
+      @figure.landmarks << @landmark
+    end
+    redirect "/figures/#{@figure.id}"
+  end
+
+  delete '/figures/:id' do
+    @figure = current_figure
+    @figure.titles.each{|title| title.figures.delete(@figure)}
+    @figure.landmarks.each{|landmark| landmark.figure = nil}
+    current_figure.destroy
+    redirect '/figures'
+  end
+
+  helpers do
+    def current_figure
+      Figure.find(params[:id])
+    end
+  end
+
 end
--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -1,3 +1,38 @@
 class LandmarksController < ApplicationController
-  # add controller methods
+  get '/landmarks' do
+    @landmarks = Landmark.all
+    erb :'/landmarks/index'
+  end
+
+  get '/landmarks/new' do
+    @titles
+    erb :'/landmarks/new'
+  end
+
+  get '/landmarks/:id' do
+    set_landmark
+    erb :'/landmarks/show'
+  end
+
+  get '/landmarks/:id/edit' do
+    set_landmark
+    erb :'/landmarks/edit'
+  end
+
+  patch '/landmarks/:id' do
+    set_landmark.update(params[:landmark])
+    redirect "/landmarks/#{@landmark.id}"
+  end
+
+  post '/landmarks' do
+    @landmark = Landmark.create(params[:landmark])
+    redirect "/landmarks/#{@landmark.id}"
+  end
+
+  private
+
+  def set_landmark
+    @landmark = Landmark.find(params[:id])
+  end
+
 end
